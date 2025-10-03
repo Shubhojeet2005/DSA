cdd50cda775e3823ca88aa551472523a76211762
shubhojeet <shubhojeetghosh2005@gmail.com>
2025-10-03 16:43:18 +0530

    avl rotation update

---
+++ AVL/rotation.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+    struct Node *lchild;
+    int data;
+    int height;
+    struct Node *rchild;
+
+} *root=NULL;
+
+int NodeHeight(struct Node *p) {
+    int hl,hr;
+    hl=p && p->lchild?p->lchild->height:0;
+    hr= p && p->rchild?p->rchild->height:0;
+
+    return hl>hr?hl+1:hr+1;
+
+}
+
+int BalanceFactor(struct Node *p) {
+    int hl,hr;
+     hl=p && p->lchild?p->lchild->height:0;
+    hr= p && p->rchild?p->rchild->height:0;
+
+    return hl-hr;
+    
+}
+
+struct Node *LLRotation(struct Node *p) {
+    struct Node *pl=p->lchild;
+    struct Node *plr = pl->rchild;
+
+    pl->rchild=p;
+    p->lchild=plr;
+    p->height=NodeHeight(p);
+    pl->height=NodeHeight(pl);
+
+    if(root==p) 
+    root=pl;
+
+    return pl;
+
+}
+
+struct Node *RRRotation(struct Node *p) {
+    struct Node *pr=p->rchild;
+    struct Node *prl = pr->lchild;
+
+    pr->lchild=p;
+    p->rchild=prl;
+    p->height=NodeHeight(p);
+    pr->height=NodeHeight(pr);
+
+    if(root==p) 
+    root=pr;
+
+    return pr;
+}
+
+struct Node *LRRotation(struct Node *p) {
+    p->lchild = RRRotation(p->lchild);
+    return LLRotation(p);
+}
+
+struct Node *RLRotation(struct Node *p) {
+    p->rchild = LLRotation(p->rchild);
+    return RRRotation(p);
+}
+
+struct Node *RInsert(struct Node *p,int key) {
+    struct Node *t=NULL;
+
+    if(p==NULL) {
+        t= new Node;
+        t->data=key;
+        t->height=1;
+        t->lchild=t->rchild=NULL;
+        return t;
+    }
+
+    if(key < p->data) {
+        p->lchild = RInsert(p->lchild, key);
+    } else if (key > p->data) {
+        p->rchild = RInsert(p->rchild, key);
+    }
+    
+    p->height=NodeHeight(p);
+
+    if(BalanceFactor(p)==2 && BalanceFactor(p->lchild)==1)
+        return LLRotation(p);
+    else if(BalanceFactor(p)==2 && BalanceFactor(p->lchild)==-1)
+        return LRRotation(p);  
+    else if(BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==-1)
+        return RRRotation(p);
+    else if(BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==1)
+        return RLRotation(p);
+
+    return p;
+}
+
